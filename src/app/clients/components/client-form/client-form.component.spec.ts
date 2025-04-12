92e076c009f173ac2163986b25583b903c568b26
RodrigoEmerson <rodrigo_belon@hotmail.com>
2025-04-12 11:33:38 -0300

    primeiro commit

--- src/app/clients/components/client-form/client-form.component.spec.ts
+++ src/app/clients/components/client-form/client-form.component.spec.ts
@@ -21,3 +21,26 @@ describe('ClientFormComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientFormComponent } from './client-form.component';
+
+describe('ClientFormComponent', () => {
+  let component: ClientFormComponent;
+  let fixture: ComponentFixture<ClientFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClientFormComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ClientFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
